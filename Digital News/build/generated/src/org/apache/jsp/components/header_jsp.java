8fdf8331e4da87c20f2f7510bc3a5c39adbe5fc3
=ductrung1901 <=ductrung1999@gmail.com>
2019-10-08 12:52:37 +0700

    first commit

---
+++ Digital News/build/generated/src/org/apache/jsp/components/header_jsp.java
@@ -0,0 +1,83 @@
+package org.apache.jsp.components;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class header_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    \n");
+      out.write("    <body>\n");
+      out.write("        <!--Header-->\n");
+      out.write("        <div>\n");
+      out.write("            <div class=\"pre-header\">\n");
+      out.write("                <img class=\"img-preheader\" src=\"public/image/preheader.jpg\">\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"banner\">\n");
+      out.write("                <div class=\"container\">\n");
+      out.write("                    <p class=\"pt-2 pl-5 text-white text-4 font-bold mt-0\">My Digital News</p>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("            <nav class=\"navigation\"><div class=\"container\">\n");
+      out.write("                    <ul class=\"list-reset my-0 py-2\">\n");
+      out.write("                        <li class=\"text-white pl-5 font-bold\">\n");
+      out.write("                            <a href=\"home\" class=\"text-white in-underline\">News</a>\n");
+      out.write("                    </ul>\n");
+      out.write("                </div>\n");
+      out.write("            </nav>\n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
